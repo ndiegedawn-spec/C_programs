50fba6e3f8b8377b5bafc176577f19ae43b00e35
ndiegedawn-spec <ndiegedawn@gmail.com>
2025-11-06 09:57:12 +0300

    Add files via upload

---
+++ program to display numbers in descending order.cxx
@@ -0,0 +1,19 @@
+/*
+Gabriel Dawn 
+Reg no:PA106/G/28836
+program to display numbers between 100 and 50 in descending order
+*/
+#include<stdio.h>
+
+
+int main()
+
+{
+	int i;
+	for ( i=100 ; i>=50; 
+	i--)
+	
+	{printf("\n %d " ,i);}
+	return 0;
+	
+}
